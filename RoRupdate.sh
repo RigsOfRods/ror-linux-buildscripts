aafe18eb2c338e9b198e94df951d6dca0e509ef7
Niklas Kersten <nkersten.dev@gmail.com>
2015-01-09 19:06:47 +0100

    updated for GitHub

--- RoRupdate.sh
+++ RoRupdate.sh
@@ -1,9 +1,6 @@
-cd ~/ror-codehg
-hg pull
-hg update
+cd ~/rigs-of-rods
+git pull
  
-rm -f CMakeCache.txt
-rm -rf CmakeFiles/
 cmake -DROR_USE_MYGUI="TRUE" \
 -DROR_USE_OPENAL="TRUE" \
 -DROR_USE_SOCKETW="TRUE" \
@@ -11,24 +8,11 @@ cmake -DROR_USE_MYGUI="TRUE" \
 -DROR_USE_CAELUM="TRUE" \
 -DROR_USE_ANGELSCRIPT="TRUE" \
 -DCMAKE_BUILD_TYPE=RELEASE \
--DCMAKE_CXX_FLAGS="-std=c++11 -O2 -march=native -pipe" \
+-DCMAKE_CXX_FLAGS="-O2 -pipe -march=native" \
 .
 
-#for optimization flags see RoRcore.sh
-
-#tell ld to use libboost_system library
-cd source/configurator/
-for link in `find . -name 'link.txt'` 
-	do
-		eval "sed '1 s/$/ -lboost_system/' -i $link"
-	done
-cd ../main/
-for link in `find . -name 'link.txt'` 
-	do
-		eval "sed '1 s/$/ -lboost_system/' -i $link"
-	done
-cd ../../
- 
 make -j$(grep -c processor /proc/cpuinfo)
 
-echo "$(tput setaf 1)NOTE: This script does not check for errors, please scroll up and check if something went wrong.$(tput sgr 0)"
+echo "$(tput setaf 1)NOTE: This script does not check for errors, please scroll up and check if something went wrong."
+echo "NOTE: Do not forget to run RoRConfig once before RoR."
+echo "NOTE: Binaries are in ~/rigs-of-rods/bin$(tput sgr 0)"

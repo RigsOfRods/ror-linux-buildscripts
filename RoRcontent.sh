b6b0e7cbdb5b370ee324dccea66ebfd115399aea
Niklas Kersten <nkersten.dev@gmail.com>
2015-01-13 19:11:03 +0100

    fixed unzipping of content packs

--- RoRcontent.sh
+++ RoRcontent.sh
@@ -10,8 +10,8 @@ while true; do
 input: " input
 
 case $input in
-        [1]* ) wget http://www.rigsofrods.com/repository/viewTag/id:981/download:1 -O content-pack-0.4.zip; unzip content-pack-0.4.zip -d ~/.rigsofrods/packs/; mv   ~/.rigsofrods/packs/streams/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/streams;;
-        [2]* ) wget http://www.rigsofrods.com/repository/downloadCDN/pack_highquality04.zip -O hq-pack-0.4.zip; unzip hq-pack-0.4.zip -d ~/.rigsofrods/packs/; mv   ~/.rigsofrods/packs/streams/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/streams;;
+        [1]* ) wget http://www.rigsofrods.com/repository/viewTag/id:981/download:1 -O content-pack-0.4.zip; unzip content-pack-0.4.zip -d ~/.rigsofrods/packs/; mv ~/.rigsofrods/packs/ContentPack04/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/ContentPack04;;
+        [2]* ) wget http://www.rigsofrods.com/repository/viewTag/id:982/download:1 -O hq-pack-0.4.zip; unzip hq-pack-0.4.zip -d ~/.rigsofrods/packs/; mv ~/.rigsofrods/packs/HighQuality04/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/HighQuality04;;
         [3]* ) exit;;
         * ) echo "Please answer 1,2 or 3";;
     esac
@@ -20,4 +20,4 @@ done
 echo "You can find more content at http://www.rigsofrods.com/repository/"
 echo "Some content may only be found in the forum: http://www.rigsofrods.com/forums/103-Showrooms-and-WIP"
 echo "Also check out this list of additional content of high quality: http://www.rigsofrods.com/wiki/pages/High_Quality_Content_not_included_in_the_HQ_Pack"
-echo "You may want to delete the downloaded .zips"
+echo "You may want to delete the downloaded .zips in your home folder"

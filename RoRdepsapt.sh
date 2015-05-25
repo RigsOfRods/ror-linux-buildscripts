562c54b9e495f374dfa8d2fd4d25df0134def5fb
Piotr Pawlow <pp@siedziba.pl>
2015-05-25 14:10:46 +0200

    Add wget to system dependencies.

--- RoRdepsapt.sh
+++ RoRdepsapt.sh
@@ -7,4 +7,4 @@ libfreetype6-dev libfreeimage-dev libzzip-dev libois-dev \
 libgl1-mesa-dev libglu1-mesa-dev nvidia-cg-toolkit libopenal-dev  \
 libx11-dev libxt-dev libxaw7-dev libxrandr-dev \
 libssl-dev libcurl4-openssl-dev libgtk2.0-dev libwxgtk3.0-dev \
-libasound2-dev libpulse-dev
+libasound2-dev libpulse-dev wget

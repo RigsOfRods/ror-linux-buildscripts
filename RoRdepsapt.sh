bd8424eaf953e491e58d9acc49a56185ef1603c3
Niklas Kersten <nkersten.dev@gmail.com>
2015-09-17 18:08:36 +0200

    added mercurial to dependencies

--- RoRdepsapt.sh
+++ RoRdepsapt.sh
@@ -2,7 +2,7 @@
 
 #Precompiled dependencies
 sudo apt-get update
-sudo apt-get -q install build-essential git cmake pkg-config libboost-all-dev \
+sudo apt-get -q install build-essential git cmake pkg-config libboost-all-dev mercurial \
 libfreetype6-dev libfreeimage-dev libzzip-dev libois-dev \
 libgl1-mesa-dev libglu1-mesa-dev nvidia-cg-toolkit libopenal-dev  \
 libx11-dev libxt-dev libxaw7-dev libxrandr-dev \

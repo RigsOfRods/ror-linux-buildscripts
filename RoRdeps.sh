d9bd623bef7357bf1e98e9b5d1d5b8f9d86ac2e3
Niklas Kersten <nkersten.dev@gmail.com>
2016-02-26 16:28:19 +0100

    use ogre and mygui from repo

--- RoRdeps.sh
+++ RoRdeps.sh
@@ -7,43 +7,6 @@ if [ ! -e "$ROR_SOURCE_DIR" ]; then
   mkdir -p "$ROR_SOURCE_DIR"
 fi
 
-# OGRE
-# TODO
-#-DOGRE_BUILD_RENDERSYSTEM_GLES2=ON \
-#-DEGL_INCLUDE_DIR="/opt/vc/include/EGL" \
-#-DOPENGLES2_INCLUDE_DIR="/opt/vc/include/GLES2" \
-
-cd "$ROR_SOURCE_DIR"
-if [ ! -e ogre ]; then
-  hg clone https://bitbucket.org/sinbad/ogre -b v1-9
-fi
-cd ogre
-hg pull -r v1-9 && hg update
-cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
--DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DOGRE_BUILD_SAMPLES:BOOL=OFF .
-make $ROR_MAKEOPTS
-make install
-
-# MyGUI
-cd "$ROR_SOURCE_DIR"
-if [ ! -e mygui ]; then
-  git clone https://github.com/MyGUI/mygui
-fi
-cd mygui
-git pull
-cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
--DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DMYGUI_BUILD_DEMOS:BOOL=OFF \
--DMYGUI_BUILD_DOCS:BOOL=OFF \
--DMYGUI_BUILD_TEST_APP:BOOL=OFF \
--DMYGUI_BUILD_TOOLS:BOOL=OFF \
--DMYGUI_BUILD_PLUGINS:BOOL=OFF .
-make $ROR_MAKEOPTS
-make install
-
 # Paged Geometry
 cd "$ROR_SOURCE_DIR"
 if [ ! -e ogre-paged ]; then
@@ -67,20 +30,4 @@ git pull
 sed -i '/^PREFIX *=/d' Makefile.conf
 make $ROR_MAKEOPTS shared
 PREFIX="$ROR_INSTALL_DIR" make install
-
-#Angelscript
-cd "$ROR_SOURCE_DIR"
-if [ ! -e angelscript ]; then
-  mkdir angelscript
-fi
-cd angelscript
-wget -c http://www.angelcode.com/angelscript/sdk/files/angelscript_2.22.1.zip
-unzip -o angelscript_*.zip
-cd sdk/angelscript/projects/gnuc
-sed -i '/^LOCAL *=/d' makefile
-# make fails when making the symbolic link, this removes the existing versions
-rm -f ../../lib/*
-SHARED=1 VERSION=2.22.1 make $ROR_MAKEOPTS
-rm -f ../../lib/*
-SHARED=1 VERSION=2.22.1 LOCAL="$ROR_INSTALL_DIR" make -s install
 
--- RoRdepsapt.sh
+++ RoRdepsapt.sh
@@ -2,9 +2,10 @@
 
 #Precompiled dependencies
 sudo apt-get update
-sudo apt-get -q install build-essential git cmake pkg-config libboost-all-dev mercurial \
-libfreetype6-dev libfreeimage-dev libzzip-dev libois-dev \
-libgl1-mesa-dev libglu1-mesa-dev nvidia-cg-toolkit libopenal-dev  \
+sudo apt-get -q install build-essential git cmake pkg-config libboost-all-dev \
+libfreetype6-dev libfreeimage-dev libzzip-dev \
+libois-dev libogre-1.9-dev libmygui-dev \
+libgl1-mesa-dev libglu1-mesa-dev libopenal-dev  \
 libx11-dev libxt-dev libxaw7-dev libxrandr-dev \
 libssl-dev libcurl4-openssl-dev libgtk2.0-dev libwxgtk3.0-dev \
-libasound2-dev libpulse-dev wget
+wget

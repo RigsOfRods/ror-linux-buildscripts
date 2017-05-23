95a3aae682a7ccbac05b9067036fc926fa63c833
Michael10055 <Michael10055@users.noreply.github.com>
2017-05-23 15:54:47 -0400

    Link fixes

--- content.sh
+++ content.sh
@@ -12,14 +12,14 @@ while true; do
 input: " input
 
 case $input in
-        [1]* ) wget http://downloadcdn.rigsofrods.org/pack_contentpack04.zip -O content-pack-0.4.zip; unzip content-pack-0.4.zip -d ~/.rigsofrods/packs/; mv ~/.rigsofrods/packs/ContentPack04/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/ContentPack04; rm content-pack-0.4.zip;;
-        [2]* ) wget http://downloadcdn.rigsofrods.org/pack_highquality04.zip -O hq-pack-0.4.zip; unzip hq-pack-0.4.zip -d ~/.rigsofrods/packs/; mv ~/.rigsofrods/packs/HighQuality04/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/HighQuality04; rm hq-pack-0.4.zip;;
+        [1]* ) wget http://dl.rigsofrods.org/pack_contentpack04.zip -O content-pack-0.4.zip; unzip content-pack-0.4.zip -d ~/.rigsofrods/packs/; mv ~/.rigsofrods/packs/ContentPack04/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/ContentPack04; rm content-pack-0.4.zip;;
+        [2]* ) wget http://dl.rigsofrods.org/pack_highquality04.zip -O hq-pack-0.4.zip; unzip hq-pack-0.4.zip -d ~/.rigsofrods/packs/; mv ~/.rigsofrods/packs/HighQuality04/* -t ~/.rigsofrods/packs/; rmdir ~/.rigsofrods/packs/HighQuality04; rm hq-pack-0.4.zip;;
         [3]* ) exit;;
         * ) echo "Please answer 1,2 or 3";;
     esac
 done
 
 echo "$(tput setaf 2)Pack successfully installed."
-echo "You can find more content at https://repofiles.avrintech.net/"
-echo "Some content may only be found in the forum: http://forum.rigsofrods.org/forum-16.html"
+echo "You can find more content at https://archives.rigsofrods.org/repo/"
+echo "Some content may only be found in the forum: https://forum.rigsofrods.org/forum-16.html"
 echo "Place new mods in ~/.rigsofrods/packs$(tput sgr 0)"

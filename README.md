4be50833b95b542c0f5f45aa44533b44c6a48ae9
Edgar <edgar@anotherfoxguy.com>
2017-10-18 11:36:10 +0200

    :bug: Fixed missing dependencies

--- README.md
+++ README.md
@@ -5,17 +5,16 @@ With some minor adjustments they should work on Linux distributions not based on
 ### Build and install Rigs of Rods:
 Run these commands from console in this order and not simultaneously:
 ```sh
-sh dependencies_apt.sh
 sh dependencies.sh
 sh core.sh
 sh content.sh
 # if you want to you can also install the community-map, but it's still WIP
 sh community_map.sh
 ```
-  
+
 ##### Notes:
-* `dependencies_apt.sh` will first ask for sudo password and will prompt you a bit later to confirm the installation of the packages. Make sure the packages don't conflict with something you need (usually doesn't happen).
-* `dependencies.sh` and `core.sh` don't require user interaction.  
+* `dependencies.sh` will first ask for sudo password and will prompt you a bit later to confirm the installation of the packages. Make sure the packages don't conflict with something you need (usually doesn't happen).
+* `core.sh` don't require user interaction.  
 * `content.sh` only requires you to choose the content you want to download in the beginning.
 * To change default directories and *make* parameters edit the `config` file (optional).
 * Depending on your hardware and internet speed this can take quite a while. As long as the scripts are running everything is fine. If an errors occurs they will stop and state the error.
@@ -27,11 +26,11 @@ simply re-run
 sh core.sh
 ```
 
-##### General notes: 
+##### General notes:
 * Included is a script for profiling. If you don't know what that means then it is not for you!
 * Scripts to build the RoR multiplayer server and set up a development environment for the RoR website named `server.sh` and `website.sh`  respecitvely are included as well (see the scripts for additional information).
 * If for whatever reason you copy parts of the script into the console make sure to set configuration
-variables appropriately, either manually, or by *sourcing* the `config` file like this: 
+variables appropriately, either manually, or by *sourcing* the `config` file like this:
 ```
 . ./config
 ```
@@ -49,7 +48,7 @@ rm -r ~/rigs-of-rods
 rm -r ~/.rigsofrods
 
 # uninstall packages of dependencies
-# replace X Y Z with the packages installed by RoRdepsapt.sh
+# replace X Y Z with the packages installed by dependencies.sh
 # note: make sure you only remove packages you don't need for something else
 sudo apt-get purge X Y Z
 
--- dependencies.sh
+++ dependencies.sh
@@ -1,119 +1,27 @@
 #!/bin/sh
 set -eu
-. ./config
 
-# Initialization
-if [ ! -e "$ROR_SOURCE_DIR" ]; then
-  mkdir -p "$ROR_SOURCE_DIR"
-fi
+# Precompiled dependencies
+sudo apt-get update
+sudo apt-get -q install build-essential wget git wget cmake pkg-config python3 \
+libogre-1.9-dev libmygui-dev libmygui.ogreplatform0debian1v5 libopenal-dev \
+libcurl4-openssl-dev libgtk2.0-dev libois-dev libssl-dev libwxgtk3.0-dev \
+libssl-dev libcurl4-openssl-dev libgtk2.0-dev libwxgtk3.0-dev
 
-# OGRE
-cd "$ROR_SOURCE_DIR"
-if [ ! -e ogre ]; then
-  hg clone https://bitbucket.org/sinbad/ogre -b v1-9
-fi
-cd ogre
-hg pull -r v1-9 && hg update
-cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
--DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=ON \
--DOGRE_BUILD_SAMPLES:BOOL=OFF \
--DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DCMAKE_CXX_FLAGS="-march=native" .
-make $ROR_MAKEOPTS
-make install
+# Dependencies not found on official repos
+cd /tmp/
+sudo wget -nv http://ftp.debian.org/debian/pool/main/r/rapidjson/rapidjson-dev_1.1.0+dfsg-3_all.deb \
+http://prdownloads.sourceforge.net/rigs-of-rods/pagedgeometry-1.2.0-x86_64.deb \
+http://prdownloads.sourceforge.net/rigs-of-rods/socketw-3.10.26-x86_64.deb \
+http://prdownloads.sourceforge.net/rigs-of-rods/angelscript-2.31.2-x86_64.deb \
+http://prdownloads.sourceforge.net/rigs-of-rods/caelum-0.6.3-x86_64.deb
 
-# OpenAL
-cd "$ROR_SOURCE_DIR"
-wget -c http://kcat.strangesoft.net/openal-releases/openal-soft-1.16.0.tar.bz2
-tar -xvjf openal-soft-1.16.0.tar.bz2
-cd openal-soft-1.16.0
-cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DCMAKE_CXX_FLAGS="-march=native" .
-make $ROR_MAKEOPTS
-make install
+sudo dpkg -i rapidjson-dev_1.1.0+dfsg-3_all.deb \
+pagedgeometry-1.2.0-x86_64.deb \
+socketw-3.10.26-x86_64.deb \
+angelscript-2.31.2-x86_64.deb \
+caelum-0.6.3-x86_64.deb
 
-# MyGUI
-cd "$ROR_SOURCE_DIR"
-if [ ! -e mygui ]; then
-  git clone https://github.com/MyGUI/mygui
-fi
-cd mygui
-git pull
-cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
--DMYGUI_BUILD_DEMOS:BOOL=OFF \
--DMYGUI_BUILD_DOCS:BOOL=OFF \
--DMYGUI_BUILD_TEST_APP:BOOL=OFF \
--DMYGUI_BUILD_TOOLS:BOOL=OFF \
--DMYGUI_BUILD_PLUGINS:BOOL=OFF \
--DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DCMAKE_CXX_FLAGS="-march=native" .
-make $ROR_MAKEOPTS
-make install
-
-# Paged Geometry
-cd "$ROR_SOURCE_DIR"
-if [ ! -e ogre-paged ]; then
-  git clone https://github.com/Hiradur/ogre-paged.git
-fi
-cd ogre-paged
-git pull
-cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DPAGEDGEOMETRY_BUILD_SAMPLES:BOOL=OFF \
--DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DCMAKE_CXX_FLAGS="-march=native" .
-make $ROR_MAKEOPTS
-make install
-
-# Caelum
-cd "$ROR_SOURCE_DIR"
-if [ ! -e ogre-caelum ]; then
-  git clone https://github.com/RigsOfRods/ogre-caelum.git
-fi
-cd ogre-caelum
-git pull
-cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DCMAKE_CXX_FLAGS="-march=native" .
-make $ROR_MAKEOPTS
-make install
-# important step, so the plugin can load:
-ln -sf "$ROR_INSTALL_DIR/lib/libCaelum.so" "$ROR_INSTALL_DIR/lib/OGRE/"
-
-# MySocketW
-cd "$ROR_SOURCE_DIR"
-if [ ! -e mysocketw ]; then
-  git clone https://github.com/Hiradur/mysocketw.git
-fi
-cd mysocketw
-git pull
-sed -i '/^PREFIX *=/d' Makefile.conf
-make $ROR_MAKEOPTS shared
-PREFIX="$ROR_INSTALL_DIR" make install
-
-# Angelscript
-cd "$ROR_SOURCE_DIR"
-if [ ! -e angelscript ]; then
-  mkdir angelscript
-fi
-cd angelscript
-wget -c http://www.angelcode.com/angelscript/sdk/files/angelscript_2.22.1.zip
-unzip -o angelscript_*.zip
-cd sdk/angelscript/projects/gnuc
-sed -i '/^LOCAL *=/d' makefile
-# make fails when making the symbolic link, this removes the existing versions
-rm -f ../../lib/*
-SHARED=1 VERSION=2.22.1 make $ROR_MAKEOPTS
-rm -f ../../lib/*
-SHARED=1 VERSION=2.22.1 LOCAL="$ROR_INSTALL_DIR" make -s install
-
-#Hydrax (included in RoR's source tree)
-#git clone --depth=1 https://github.com/imperative/CommunityHydrax.git
-#cd CommunityHydrax
-#make -s -j$cpucount PREFIX=/usr/local
-#sudo make install PREFIX=/usr/local
-#cd ..
 
 echo "$(tput setaf 2)All dependencies were installed successfully"
 echo "You can now proceed with core.sh$(tput sgr 0)"

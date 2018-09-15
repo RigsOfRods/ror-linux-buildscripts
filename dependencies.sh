4d53e33874882e4bea4b3fd3fceb19cf9f30366c
Edgar <Edgar@AnotherFoxGuy.com>
2018-09-15 11:12:24 +0200

    :tada: Update for conan

--- dependencies.sh
+++ dependencies.sh
@@ -4,27 +4,22 @@ set -eu
 
 # Precompiled dependencies
 sudo apt-get update
-sudo apt-get -q install build-essential git cmake pkg-config libboost-all-dev mercurial \
+sudo apt-get -q install build-essential git cmake pkg-config libboost-all-dev ninja-build \
 libfreetype6-dev libfreeimage-dev libzzip-dev libois-dev \
 libgl1-mesa-dev libglu1-mesa-dev nvidia-cg-toolkit libopenal-dev  \
 libx11-dev libxt-dev libxaw7-dev libxrandr-dev \
 libssl-dev libcurl4-openssl-dev libgtk2.0-dev libwxgtk3.0-dev \
-libasound2-dev libpulse-dev wget
+libasound2-dev libpulse-dev wget python3-pip
 
-# Initialization
-if [ ! -e "$ROR_SOURCE_DIR" ]; then
-  mkdir -p "$ROR_SOURCE_DIR"
-fi
+# Conan
+cd /tmp/
+sudo wget -nv https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_7_3.deb
+sudo dpkg -i conan-ubuntu-64_1_7_3.deb
 
-cd "$ROR_SOURCE_DIR"
-if [ ! -e ror-dependencies ]; then
-  git clone https://github.com/${YOUR_GITHUB_USERNAME}/ror-dependencies.git
-fi
-cd ror-dependencies
-git pull
+conan user
 
-cmake -DBUILD_CAELUM=OFF .
-make $ROR_MAKEOPTS
+conan remote add ror-dependencies https://api.bintray.com/conan/anotherfoxguy/ror-dependencies
+conan remote add bincrafters-public-conan https://api.bintray.com/conan/bincrafters/public-conan
 
 echo "$(tput setaf 2)All dependencies were installed and build successfully."
 echo "You can now proceed with game.sh$(tput sgr 0)"

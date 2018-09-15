ddccda1652d65a196546c2ec312e68485e515d87
Edgar <Edgar@AnotherFoxGuy.com>
2018-09-15 11:12:24 +0200

    :tada: Update for conan

--- game.sh
+++ game.sh
@@ -19,17 +19,13 @@ if [ ! -e "0_build/$CMAKEBUILDTYPE" ]; then
 fi
 cd 0_build/$CMAKEBUILDTYPE
 
-ROR_CMAKE_ARGS=""
-if [ -e "$ROR_SOURCE_DIR/ror-dependencies" ]; then
-    ROR_CMAKE_ARGS=-DCMAKE_PREFIX_PATH=$ROR_SOURCE_DIR/ror-dependencies/Dependencies_Linux/
-fi
+conan install --build=missing -u ../../
 
-cmake ../../ \
+cmake ../../ -GNinja \
 -DCMAKE_BUILD_TYPE=$CMAKEBUILDTYPE \
 -DBUILD_CUSTOM_VERSION=ON \
 -DCUSTOM_VERSION="0.4.8.0-shscript" \
--DCMAKE_CXX_FLAGS="-pipe -march=native" \
-$ROR_CMAKE_ARGS
+-DCMAKE_CXX_FLAGS="-pipe -march=native"
 
 # CMAKE_CXX_FLAGS (flags for compiler) - Default are:
 # -march=native  -- Optimize RoR for the CPU the build is performed on; executable will only be portable to systems with same or newer CPU architecture
@@ -39,11 +35,10 @@ $ROR_CMAKE_ARGS
 # -DCMAKE_CXX_FLAGS="-Ofast -march=native -pipe -flto -mfpmath=both -funroll-loops -floop-parallelize-all -ftree-parallelize-loops=4" \
 
 
-make $ROR_MAKEOPTS
-make zip_and_copy_resources
-sudo make install
+ninja
+ninja zip_and_copy_resources
+sudo ninja install
 
 echo "$(tput setaf 2)Build succeeded!"
 echo "NOTE: Do not forget to run RoRConfig once before RoR."
 echo "NOTE: Mods belong in ~/.rigsofrods/packs"
-echo "NOTE: Run content.sh if you want to install the Content Pack$(tput sgr 0)"
